9d52e21ee5e62f880479fae7062889017f570890
YifanCui <18810669189@163.com>
2022-11-15 23:00:56 +0800

    脚手架修改默认配置

---
+++ Vue2/component/3_SingleComponent/src/components/MySchool.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--  组件的结构-->
+  <div class="demo">
+    <p>name:{{name}}</p>
+    <p>address:{{address}}</p>
+    <button @click="showName">show MySchool name</button>
+  </div>
+</template>
+
+<script>
+  // 组件交互相关代码（数据/方法等等）
+  export default {
+    name:'MySchool',
+    data(){
+      return {
+        name:'University of Business and Economy',
+        address:'Alaska'
+      }
+    },
+    methods:{
+      showName(){
+        console.log('###',this)
+      }
+    }
+  }
+</script>
+
+<style>
+  /*组件的样式*/
+  .demo{
+    background-color: orange;
+  }
+</style>
---
+++ Vue2/component/3_SingleComponent/vue.config.js
@@ -0,0 +1,14 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+    },
+  },
+  lintOnSave:false
+}
