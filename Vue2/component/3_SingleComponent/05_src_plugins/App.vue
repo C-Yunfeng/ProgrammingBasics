60aa21f59691947a2d6662740ff52ad7d368618e
YifanCui <18810669189@163.com>
2022-11-19 00:59:18 +0800

    attributes_plugins

---
+++ Vue2/component/3_SingleComponent/05_src_plugins/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!--此处的标签无效，因为没有配置props-->
+    <Student name="Alex" :age="19"></Student>
+    <School></School>
+  </div>
+</template>
+
+<script>
+  import Student from "@/components/Student";
+  import School from "@/components/School";
+  export default {
+    name: "App",
+    components:{
+      Student,School
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
