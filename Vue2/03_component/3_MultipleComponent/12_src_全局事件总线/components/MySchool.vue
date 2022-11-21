801cfaeca38327af0e018097350de4313db51cc6
YifanCui <18810669189@163.com>
2022-11-22 01:25:49 +0800

    全局事件总线

---
+++ Vue2/03_component/3_MultipleComponent/12_src_全局事件总线/components/MySchool.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="school">
+		<h2>学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'MySchool',
+		data() {
+			return {
+				name:'尚硅谷',
+				address:'北京',
+			}
+		},
+    mounted() {
+      console.log('school',this.x)
+      this.$bus.$on('hello',(data)=>{
+        console.log('This is School and received data:',data)
+      })
+    },
+    // 销毁之前解绑组件，避免占用事件hello
+    beforeDestroy() {
+      this.$bus.off('hello')
+    }
+  }
+</script>
+
+<style scoped>
+	.school{
+		background-color: skyblue;
+		padding: 5px;
+	}
+</style>
