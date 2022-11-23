fc031f97fb42f41c96ece3e47013e225d4a38a24
YifanCui <18810669189@163.com>
2022-11-23 23:26:04 +0800

    配置代理服务器
    devServer, pathRewrite, changeOrigin, axios.get

---
+++ Vue2/03_component/3_MultipleComponent/workspace_demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="getStudentName">Get student name</button>
+    <button @click="getCarName">Get cat name</button>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "App",
+    methods:{
+      getStudentName(){
+        // axios.get("http://localhost:8080/test.txt").then(
+        axios.get("http://localhost:8080/api/students").then(
+            response =>{
+              console.log('request succeed',response.data)
+            },
+            error =>{
+              console.log('request failed',error.message)
+            }
+        )
+      },
+      getCarName(){
+        // axios.get("http://localhost:8080/test.txt").then(
+        axios.get("http://localhost:8080/demo/cars").then(
+            response =>{
+              console.log('request succeed',response.data)
+            },
+            error =>{
+              console.log('request failed',error.message)
+            }
+        )
+      },
+    }
+  }
+</script>
+
--- Vue2/03_component/3_MultipleComponent/workspace_demo/vue.config.js
+++ Vue2/03_component/3_MultipleComponent/workspace_demo/vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 配置方法一
+  // devServer:{
+  //   proxy:"http://localhost:5000/"
+  // }
+  devServer:{
+    proxy:{
+      '/api':{
+        target:"http://localhost:5000/", //代理目标的基础路径
+        pathRewrite:{'^/api':''}
+        // 默认为true
+        // ws:true, //用于支持websocket
+        // changeOrigin:true //用于控制请求头中的host值
+      },
+      '/demo':{
+        target:"http://localhost:5001/", //代理目标的基础路径
+        pathRewrite:{'^/demo':''}
+        // 默认为true
+        // ws:true, //用于支持websocket
+        // changeOrigin:true //用于控制请求头中的host值
+      }
+    }
+  }
 })
