3e9e80348bc3b5e8254938479d2a98e242af3a9a
YifanCui <18810669189@163.com>
2022-12-01 00:53:07 +0800

    ElementUI使用

--- Vue2/03_component/3_MultipleComponent/workspace_demo/src/main.js
+++ Vue2/03_component/3_MultipleComponent/workspace_demo/src/main.js
@@ -1,5 +1,8 @@
 // 引入Vue
 import Vue from 'vue'
+// commit实际不推荐引入整个文件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 // 引入App
 import App from './App.vue'
 // 引入插件
@@ -9,11 +12,10 @@ import router from './router'
 
 // 关闭生产提示
 Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 // 创建vm
 new Vue({
     el:'#app',
-    render: h=>h(App),
-    router:router
+    render: h=>h(App)
 })
