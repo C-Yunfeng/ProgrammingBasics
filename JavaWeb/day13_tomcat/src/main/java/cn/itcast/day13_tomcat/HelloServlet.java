8735c8c27c398cc5917f27e5c0f125980f1536f5
YifanCui <18810669189@163.com>
2023-02-22 22:48:58 +0800

    [ADD] Servlet基本使用
    1. 概念
    2. 步骤
    3. 执行原理
    4. 生命周期
    5. Servlet3.0注解配置
    6. HttpServlet(覆写doGet和doPost)
    7. WebServlet多种访问方式

--- JavaWeb/day13_tomcat/src/main/java/cn/itcast/day13_tomcat/HelloServlet.java
+++ JavaWeb/day13_tomcat/src/main/java/cn/itcast/day13_tomcat/HelloServlet.java
@@ -2,18 +2,24 @@ package cn.itcast.day13_tomcat;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+// 3.0及以上版本不需要在web.xml里配置servlet,使用注解即可
+// @WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/demo1")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
+        // Servlet是单例模式,存在线程安全问题.因此尽量不要在servlet中定义成员变量/不赋值/final修饰
+        System.out.println("init...");
         message = "Hello World!";
     }
-
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("doGet...");
         response.setContentType("text/html");
 
         // Hello
@@ -23,6 +29,12 @@ public class HelloServlet extends HttpServlet {
         out.println("</body></html>");
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost...");
+    }
+
     public void destroy() {
+        System.out.println("destroy...");
     }
 }
