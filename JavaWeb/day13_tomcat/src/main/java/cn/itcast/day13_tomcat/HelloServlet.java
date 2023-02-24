451413ea8ccb579e46ec36a8c3b526c3db95cb2b
YifanCui <18810669189@163.com>
2023-02-24 22:20:31 +0800

    [UPD] 测试helloServlet配置

--- JavaWeb/day13_tomcat/src/main/java/cn/itcast/day13_tomcat/HelloServlet.java
+++ JavaWeb/day13_tomcat/src/main/java/cn/itcast/day13_tomcat/HelloServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.annotation.*;
 
 // 3.0及以上版本不需要在web.xml里配置servlet,使用注解即可
 // @WebServlet(name = "helloServlet", value = "/hello-servlet")
-@WebServlet("/demo1")
+@WebServlet("/*")
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
         System.out.println("init...");
         message = "Hello World!";
     }
-    @Override
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("doGet...");
         response.setContentType("text/html");
@@ -29,7 +29,6 @@ public class HelloServlet extends HttpServlet {
         out.println("</body></html>");
     }
 
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doPost...");
     }
